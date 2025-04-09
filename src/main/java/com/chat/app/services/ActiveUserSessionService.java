412eced32f8d9d59a9fad06bdda6804cef0ef239
david-degenhardt <davidegenhardt@gmail.com>
2025-04-09 09:27:10 +0200

    Removed unused imports

--- src/main/java/com/chat/app/services/ActiveUserSessionService.java
+++ src/main/java/com/chat/app/services/ActiveUserSessionService.java
@@ -1,7 +1,6 @@
 package com.chat.app.services;
 
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.stereotype.Service;
